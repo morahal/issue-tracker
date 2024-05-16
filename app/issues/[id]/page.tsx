1359a39180808e15a70f285dea2d32ad30c99d8a
Mohammad Rahal <moodyrahal@gmail.com>
2024-05-16 13:39:44 +0300

    started updating issue

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,36 @@
+import prisma from "@/prisma/client";
+import { Box, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import React from "react";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailsPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    notFound();
+  }
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+      <Box>
+        <IssueDetails issue={issue} />
+      </Box>
+
+      <Box>
+        <EditIssueButton issueId={issue.id} />
+      </Box>
+    </Grid>
+  );
+};
+
+export default IssueDetailsPage;
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,11 +1,11 @@
+import { Table } from "@radix-ui/themes";
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import prisma from "@/prisma/client";
-import { IssueStatusBadge, Link } from "@/app/components";
+import IssueStatusBadge from "../components/IssueStatusBadge";
+import { Skeleton } from "@/app/components";
 import IssueActions from "./IssueActions";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const LoadingIssuesPage = () => {
+  const issues = [1, 2, 3, 4, 5];
 
   return (
     <div>
@@ -24,18 +24,18 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <Skeleton />{" "}
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />{" "}
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />{" "}
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -45,4 +45,4 @@ const IssuesPage = async () => {
   );
 };
 
-export default IssuesPage;
+export default LoadingIssuesPage;
